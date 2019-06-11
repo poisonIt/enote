f3247b30cd6c251dd89d02b61a97f0e60876daec
poison <poisonit@163.com>
2019-06-11 14:21:59 +0800

    update and add move prompt message

--- src/components/Menu/src/Menu.vue
+++ src/components/Menu/src/Menu.vue
@@ -162,7 +162,8 @@ export default {
 
     itemSortType (item, type) {
       if (item.type === 'sort') {
-        return type === 'down' ? !item.actived : item.actived
+        // return type === 'down' ? !item.actived : item.actived
+        return type === 'down' ? item.actived : !item.actived
       } else {
         return false
       }
@@ -182,6 +183,7 @@ export default {
         group.items.forEach(item => {
           item.actived = false
         })
+        item.actived = true
       }
       this.$emit('itemClick', item.value, item, index)
       this.handleClose()

806e2f60c538d81932c0998a045daaecc2010371
bowiego <295030485@qq.com>
2019-06-11 14:01:48 +0800

    add network_status in login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -53,10 +53,13 @@ import Loading from '@/components/Loading'
 
 export default {
   name: 'Login',
+
   mixins: [ pullData ],
+
   components: {
     Loading
   },
+
   data () {
     return {
       showHeader: false,
@@ -68,6 +71,13 @@ export default {
       isLoading: false
     }
   },
+
+  computed: {
+    ...mapGetters({
+      network_status: 'GET_NETWORK_STATUS'
+    })
+  },
+
   created () {
     if (this.$remote.app.appConf.platform !== 'darwin') {
       this.showHeader = true
@@ -88,6 +98,7 @@ export default {
       ipcRenderer.send('login-ready')
     })
   },
+
   mounted () {
     if (this.autoLogin !== '1') {
       setTimeout(() => {
@@ -95,16 +106,24 @@ export default {
       }, 300)
     }
   },
+
   methods: {
     ...mapActions([
       'SET_TOKEN',
       'SET_FILES_FROM_LOCAL'
     ]),
+  
     async postInput () {
       if (this.isLoading) return
       this.isLoading = true
       const { username, password } = this
       console.log('postInput', username, password)
+      if (this.network_status === 'offline') {
+        if (this.autoLogin === '1') {
+          ipcRenderer.send('login-ready')
+          return
+        }
+      }
 
       let authenticateResp = await authenticate({
         username: username,
